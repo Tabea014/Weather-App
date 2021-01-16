b21d6b9b7783dda88b6d6496a8f07a440ec1270f
Tabea014 <76948160+Tabea014@users.noreply.github.com>
2021-01-16 18:46:48 +0100

    Add current day & time

---
+++ script.js
@@ -0,0 +1,28 @@
+let now = new Date();
+
+let h3 = document.querySelector("h3");
+
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+
+
+h3.innerHTML= `${day} ${hours}:${minutes}`;
